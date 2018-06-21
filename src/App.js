0d76a0f084e4d7fd00258c9f313ab09721843c71
AlexAdrienAuger <alex.adrien.auger@gmail.com>
2018-06-21 18:58:14 +0200

    make it more beautiful

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Card, CardContent, CardActions, Button, Typography} from '@material-ui/core';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Card style={{width: '200px', margin: '300px auto'}}>
+          <CardContent>
+            <Typography color="textSecondary">
+              Like the older ArchiQ, but better
+            </Typography>
+            <Typography variant="headline" component="h2">
+              ArchiQ2
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Be Awesome!</Button>
+          </CardActions>
+        </Card>
       </div>
     );
   }
